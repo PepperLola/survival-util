6deb12866add51bf83fe5264c5ae6fa2ad0e32a4
PepperLola <julian.orion.wright@gmail.com>
2020-10-19 14:40:38 -0700

    initial 1.16.3 commit 🚀

---
+++ src/main/java/net/themorningcompany/survivalutil/modules/util/AutoDisconnectModule.java
@@ -0,0 +1,60 @@
+package net.themorningcompany.survivalutil.modules.util;
+
+import net.themorningcompany.survivalutil.modules.Module;
+import net.themorningcompany.survivalutil.util.AFKUtil;
+import net.themorningcompany.survivalutil.util.MCUtil;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.entity.player.ClientPlayerEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.util.DamageSource;
+import net.minecraftforge.client.event.sound.SoundEvent;
+import net.minecraftforge.event.TickEvent;
+import net.minecraftforge.event.entity.living.LivingDamageEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+
+public class AutoDisconnectModule extends Module {
+
+    private boolean shouldDisconnect = false;
+    private DamageSource lastDamageSource = null;
+
+    public AutoDisconnectModule() {
+        super("auto-disconnect", "Auto-Disconnect", ModuleType.UTIL);
+    }
+
+    @SubscribeEvent
+    public void onSoundPlay(SoundEvent.SoundSourceEvent event) {
+        PlayerEntity player = Minecraft.getInstance().player;
+        if (player == null) return;
+
+        boolean isCreeper = event.getName().equals("entity.creeper.primed");
+        boolean isSurvival = MCUtil.isPlayerInSurvival(player);
+        if (isCreeper && isSurvival && AFKUtil.isAFK()) {
+            shouldDisconnect = true;
+        }
+//        System.out.println(event.getName());
+    }
+
+    @SubscribeEvent
+    public void onClientTick(TickEvent.ClientTickEvent event) {
+        PlayerEntity player = Minecraft.getInstance().player;
+        if (player == null) return;
+        double playerHealth = player.getHealth();
+        if (playerHealth < 1) {
+            MCUtil.copyDisconnectReason(player, lastDamageSource);
+            if (MCUtil.isPlayerInSurvival(player) || shouldDisconnect) {
+                MCUtil.disconnectFromServer();
+            }
+        }
+        shouldDisconnect = false;
+    }
+
+    @SubscribeEvent
+    public void onDamage(LivingDamageEvent event) {
+        ClientPlayerEntity player = Minecraft.getInstance().player;
+        if (player == null) return;
+        if (!(event.getEntity() instanceof PlayerEntity)) return;
+        if (!(event.getEntity() == player)) return;
+
+        lastDamageSource = event.getSource();
+    }
+}
---
+++ src/main/java/net/themorningcompany/survivalutil/modules/util/DurabilityNotificationModule.java
@@ -0,0 +1,45 @@
+package net.themorningcompany.survivalutil.modules.util;
+
+import net.themorningcompany.survivalutil.modules.Module;
+import net.themorningcompany.survivalutil.util.MCUtil;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.entity.player.ClientPlayerEntity;
+import net.minecraft.entity.player.PlayerInventory;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.text.TextFormatting;
+import net.minecraftforge.event.TickEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DurabilityNotificationModule extends Module {
+
+    private List<Item> warns = new ArrayList<>();
+
+    public DurabilityNotificationModule() {
+        super("durability-notification", "Durability Notifier", ModuleType.UTIL);
+    }
+
+    @SubscribeEvent
+    public void onToolUse(TickEvent.ClientTickEvent event) {
+        ClientPlayerEntity player = Minecraft.getInstance().player;
+        if (player == null) return;
+        PlayerInventory inv = player.inventory;
+
+        for (int i = 0; i < inv.getSizeInventory(); i++) {
+            ItemStack item = inv.getStackInSlot(i);
+            int itemDamage = item.getDamage();
+            if (item.getMaxDamage() == 0) return;
+            String warnString = "Your " + (item.hasDisplayName() ? String.format("\"%s\"", item.getDisplayName().getString()) : item.getItem().getName().getString()) + " is on " + (item.getMaxDamage() - itemDamage) + " durability.";
+            if (warns.contains(item.getItem()) && itemDamage >= item.getMaxDamage() - 1) {
+                MCUtil.sendPlayerMessage(player, warnString, TextFormatting.RED);
+                warns.remove(item.getItem());
+            } else if (!warns.contains(item.getItem()) && itemDamage >= item.getMaxDamage() - 10 && itemDamage < item.getMaxDamage() - 1) {
+                MCUtil.sendPlayerMessage(player, warnString, TextFormatting.RED);
+                warns.add(item.getItem());
+            }
+        }
+    }
+}
